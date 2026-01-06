2f9d93e9e00a8c8ce9c754727a060b028230e634
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-01-06 22:31:28 +0200

    Initial commit

---
+++ Hello World iOS Application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello World iOS Application
+//
+//  Created by webuxmotion on 06.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello World iOS Application/Hello_World_iOS_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_iOS_ApplicationApp.swift
+//  Hello World iOS Application
+//
+//  Created by webuxmotion on 06.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_iOS_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
